eccf1eb08372030b9ff94dbefb7163c403966316
Sayan Maity <cse21109@cemk.ac.in>
2024-08-29 22:05:22 +0530

    db index.js change src index.js changes

---
+++ src/db/index.js
@@ -0,0 +1,19 @@
+import mongoose from "mogoose";
+
+import {
+    DBNAME,
+    DBURL,
+    DBPORT,
+} from "../constants.js";
+
+const connectDB = async () => {
+
+    mongoose.connect(`${DBURL}/${DBNAME}`).then((connectionInstance) => {
+        console.log(` MONGODB Connected to database ${DBNAME} on port ${DBPORT}`);
+    }).catch((err) => {
+        console.log("Error connecting to database", err);
+        process.exit(1);
+    });
+}
+
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import app from "./app.js";
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+import {urlencoded} from "express";
+
+dotenv.config({path: "./.env"});
+
+connectDB()
+.then(()=>{
+    app.on("error", (err) => {
+        console.log("Error connecting to express server", err);
+        throw err
+    });
+    app.listen(process.env.PORT, () => {
+        console.log(`Server running on port ${process.env.PORT}`);
+    });
+}).catch((err) => {
+    console.log("Error connecting to database", err);
+    throw err;
+});
+
 
 
