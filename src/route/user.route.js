4f0ded182270317b8d1664a0ca6a94b27a6c646a
SAYAN MAITY <156517005+Sayancemk@users.noreply.github.com>
2024-08-29 15:12:13 +0000

    chore: Add Express routes for user authentication

---
+++ src/route/user.route.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import { signUp, 
+            signIn, 
+            signOut, 
+            verifyEmail, 
+            forgotPassword, 
+            resetPassword,
+            checkAuth } from '../controller/user.controller';
+
+import { verifyToken } from '../middleware/verifyToken.js';
+
+const router = express.Router();
+
+router.route('/sign-up').post(signUp);
+
+router.route('/sign-in').post(signIn);
+
+router.route('/sign-out').get(verifyToken,signOut);
+
+router.route('/verify-email').post(verifyEmail);
+
+router.route('/forgot-password').post(forgotPassword);
+
+router.route('/reset-password/:token').post(resetPassword);
+
+router.route('/check-auth').get(verifyToken,checkAuth);
+
+export default router;
