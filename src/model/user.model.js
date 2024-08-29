369958a9095016902016938b7bba750bef85ab88
SAYAN MAITY <156517005+Sayancemk@users.noreply.github.com>
2024-08-29 14:43:43 +0000

    feat: update user model with additional fields
    This commit adds the missing fields to the user model in the `user.model.js` file. The `name`, `email`, `password`, `lastLogin`, `isVerified`, `resetPasswordToken`, `resetPasswordExpires`, `verficationToken`, and `verficationTokenExpires` fields are now included in the schema. This update ensures that all required user information is properly stored and managed.

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -1,4 +1,51 @@
 import mongoose from "mongoose";
 
 
-const userSchema = new mongoose.Schema({})
+const userSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastLogin: {
+        type: Date,
+        default: Date.now
+    },
+
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+
+    resetPasswordToken: {
+        type: String
+    },
+    resetPasswordExpires: {
+        type: Date
+
+    },
+
+    verficationToken: {
+        type: String
+    },
+
+    verficationTokenExpires: {
+        type: Date
+    }
+
+}, { timestamps: true });
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
